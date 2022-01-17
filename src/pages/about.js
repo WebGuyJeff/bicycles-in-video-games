eb9850d2c3f0c551ecef59a772fdcc3ee3e209e7
wordpressjeff <me@jeffersonreal.com>
2022-01-17 13:18:39 +0000

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle="About the Bicycle Hunter">
+            <p> 
+                Hunting bicycles in games is a long-term interest of mine. The bicycle is naturally a
+                gentlemen's invention and is regarded, by those with class, to be the pinnacle of modern
+                sophistication. To discover a bicycle in a videogame is to get a deep insight into the
+                developer's psyche. Crude, or painstakingly crafted, taking the time to appreciate a VG bike
+                always brings a high-poly tear to my eye.
+            </p>
+        </Layout>
+    )
+}
+
+export default AboutPage
