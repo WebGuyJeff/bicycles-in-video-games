8a10dae45d7f1ad6c3b97c8362c1eb96734a0533
wordpressjeff <me@jeffersonreal.com>
2022-01-17 13:47:32 +0000

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,18 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
     return (
         <Layout pageTitle="Home Page">
-            <p>I'm making this by following the Gatsby Tutorial.</p>
+            <p>
+                Welcome to a digital journal of bicycles featured in video games.
+            </p>
+            <StaticImage
+                alt="Fallout 4: An early 20th century women's bicycle with full chainguard and
+                butchers front rack."
+                src="../images/fallout-4-videogame-bicycle.jpg"
+            />
         </Layout>
     )
 }
