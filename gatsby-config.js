ed2ea93d929670f91d37569d40bee70cb06d1a27
wordpressjeff <me@jeffersonreal.com>
2022-01-17 15:13:30 +0000

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,17 @@
 module.exports = {
     siteMetadata: {
-      title: `Bicycles in Videogames`,
+        title: `Bicycles in Video Games`,
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
         "gatsby-plugin-image",
         "gatsby-plugin-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: `blog`,
+                path: `${__dirname}/blog`,
+            }
+        },
     ],
 }
